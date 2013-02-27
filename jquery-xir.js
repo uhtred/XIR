5f64ee29f7cb1e152915f761f52d379b83c04a44
Daniel Ribeiro França <daniel.ura@gmail.com>
2013-02-26 21:34:18 -0300

    Url validation

--- jquery-xir.js
+++ jquery-xir.js
@@ -18,8 +18,11 @@
 
         options = $.extend( {}, defaults, options );
 
-        var req_img = new Image();
-        	req_img.src = options.url + '?' + $.param(options.data) + '&_timestamp=' + _getTimeStamp() ;
+		var req_img = new Image(),
+			req_url_sep = /\?/.test( options.url ) ? '' : '?';
+
+
+		req_img.src = options.url + req_url_sep + $.param(options.data) + '&_timestamp=' + _getTimeStamp() ;
 
     	if( options.complete ) {
 			req_img.onload = options.complete;
