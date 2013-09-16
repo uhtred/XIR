f9af4383ffd54d05016ddabcc3110266de3f6631
Daniel Ribeiro França <daniel.ura@gmail.com>
2013-09-16 14:53:52 -0300

    shrink code

--- jquery-xir.js
+++ jquery-xir.js
@@ -1,28 +1,19 @@
 ;(function ( $, window, document, undefined ) {
 
     var defaults = {
-            url: "",
-            data: {},
-            complete: function(){}
-        };
-
-    function _getTimeStamp(){
-
-    	var timestamp = new Date().getTime();
-
-		return "" + timestamp;
-
-    }
+		url: "",
+		data: { '_timestamp': ( new Date() ).getTime() },
+		complete: function(){}
+	};
 
     function xir( options ) {
 
-        options = $.extend( {}, defaults, options );
+        options = $.extend( true, {}, defaults, options );
 
 		var req_img = new Image(),
 			req_url_sep = /\?/.test( options.url ) ? '' : '?';
 
-
-		req_img.src = options.url + req_url_sep + $.param(options.data) + '&_timestamp=' + _getTimeStamp() ;
+		req_img.src = options.url + req_url_sep + $.param( options.data );
 
     	if( options.complete ) {
 			req_img.onload = options.complete;
